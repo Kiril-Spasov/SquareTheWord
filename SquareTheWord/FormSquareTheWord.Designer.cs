3066b002cf64c8865fb6a931afc888a1f51dd5a5
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-22 14:52:51 +0200

    Add project files.

---
+++ SquareTheWord/FormSquareTheWord.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace SquareTheWord
+{
+    partial class FormSquareTheWord
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnSquareTheWord = new System.Windows.Forms.Button();
+            this.TxtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnSquareTheWord
+            // 
+            this.BtnSquareTheWord.Location = new System.Drawing.Point(55, 70);
+            this.BtnSquareTheWord.Name = "BtnSquareTheWord";
+            this.BtnSquareTheWord.Size = new System.Drawing.Size(208, 65);
+            this.BtnSquareTheWord.TabIndex = 0;
+            this.BtnSquareTheWord.Text = "Square The Word";
+            this.BtnSquareTheWord.UseVisualStyleBackColor = true;
+            this.BtnSquareTheWord.Click += new System.EventHandler(this.BtnSquareTheWord_Click);
+            // 
+            // TxtResult
+            // 
+            this.TxtResult.Location = new System.Drawing.Point(325, 70);
+            this.TxtResult.Multiline = true;
+            this.TxtResult.Name = "TxtResult";
+            this.TxtResult.Size = new System.Drawing.Size(397, 320);
+            this.TxtResult.TabIndex = 1;
+            // 
+            // FormSquareTheWord
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.TxtResult);
+            this.Controls.Add(this.BtnSquareTheWord);
+            this.Name = "FormSquareTheWord";
+            this.Text = "Square The Word";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnSquareTheWord;
+        private System.Windows.Forms.TextBox TxtResult;
+    }
+}
+
---
+++ SquareTheWord/FormSquareTheWord.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SquareTheWord
+{
+    public partial class FormSquareTheWord : Form
+    {
+        public FormSquareTheWord()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSquareTheWord_Click(object sender, EventArgs e)
+        {
+            string line = "";
+
+            string path = Application.StartupPath + @"\square.txt";
+            StreamReader streamReader = new StreamReader(path);
+
+            bool finished = false;
+
+            while (!finished)
+            {
+                line = streamReader.ReadLine();
+
+                if (line == null)
+                {
+                    finished = true;
+                }
+                else
+                {
+                    SquareWord(line);
+                    TxtResult.Text += Environment.NewLine;
+                }
+            }
+        }
+
+        private void SquareWord(string word)
+        {
+            TxtResult.Text += word + Environment.NewLine;
+            string emptySpaces = "                         ";
+
+            for (int i = 1; i < word.Length - 1; i++)
+            {
+                TxtResult.Text += word.Substring(i, 1) + emptySpaces.Substring(0, word.Length - 2) + word.Substring((word.Length - 1) - i, 1) + Environment.NewLine;
+            }
+ 
+            ReverseWord(word);
+            TxtResult.Text += Environment.NewLine;
+        }
+
+        private void ReverseWord(string word)
+        {
+            for (int i = word.Length - 1; i >= 0; i--)
+            {
+                TxtResult.Text += word.Substring(i, 1);
+            }
+        }
+    }
+}
